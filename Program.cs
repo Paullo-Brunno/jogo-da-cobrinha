86d16341a482234e6b356d1e278b1da7ff270974
Paullo Brunno <paullobrunnosm@gmail.com>
2025-06-03 11:47:50 -0300

    jogo da cobrinha

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Rodando...");
+            Thread.Sleep(100); //pausa o game por 100ms
+        }
+    }
+}
+
